cdd2ff4c233f91b7a451ad6cf5026625d8e1c842
jinsu <jinsu@ezpmp.co.kr>
2022-01-25 16:39:59 +0900

    first commit

---
+++ src/main/java/com/sample/api/SampleApiApplication.java
@@ -0,0 +1,26 @@
+package com.sample.api;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@OpenAPIDefinition(
+    info = @Info(
+        title="Sample API",
+        version = "1.0"
+	)
+)
+@EnableJpaAuditing
+public class SampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApiApplication.class, args);
+	}
+}
